451aa004184aacb2915f79d0d69cfd4b1eb48fc6
Rosh <rosh_408@hotmail.com>
2019-05-01 22:30:40 -0700

    Add Tablet/Desktop Version to Media Queries

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-function sayHello () {
-    console.log('helljnhjbhjbhjbbh');
-}
-sayHello();
-
-console.log('testing');
-console.log('hello dojdncs');
+$.ajax({
+    method: 'get',
+    url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=n8C2eF8jDItIXtbM7nYFtYbPiZax89hp'
+  })
+    .done(function(data){
+    console.log(data);
+  })
