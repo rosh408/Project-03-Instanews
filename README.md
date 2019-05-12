2ddc9c209bdaec315647959f6a26cb01b5abf8e9
Rosh <rosh_408@hotmail.com>
2019-05-12 14:00:31 -0700

    Fully complete README.md // Describe personal learnings and Installations

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
-# Instanews App 
+# Instanews App | Project 03
 
 ## index.html 
 
-Contains: 
+Personal learnings: 
+- Adding html text to the webpage doesn't only have to be written in index.html, jQuery can also do this.
+- Having a clean, indented html is important due to a better visualization of the webpage as well as having a better grasp of what every element's purpose is.
+- Adding alt attributes and meta tags (with name="Description" and content="") helped boosted my SEO on Lighthouse Audits.
+- Adding script tags to the bottom of the body tag is important because it lets the browser go through the markup language first before it reaches the back-end functions or script.js.
 
-## _main.scss
+## _main.scss/stylesheet
 
-Contains: 
+Personal Learnings:
+- Applying scss to Instanews made me realize the convenience of scss through it's unique style of nesting elements.
+- Using @include to target tablet/desktop widths. 
+- Designing a jQuery plug in requires a new a scss file (e.g. _selectrics.scss)
 
 ## script.js
 
-Contains: 
+Personal learnings: 
+- Using the slice() and filter() methods gave me a better understanding of how the browser was grabbing each individual item from the API. 
+- Adding multiple css styling to elements through jQuery is a huge hassle and it is better to use the addClass() methods and styling them later in _main.scss.
+- Inserting a dynamic variable (category) in the New York Times URL was a useful part of the API process because it would automatically grab the right article images and texts from new york times according to what was clicked ( const category = $(this).val(); )
 
-What I've learned:
 
+## Installations 
+
+- Installed a jQuery plug in that gave my dropdown menu bar some styling which came with it.
+- This plug in requires that I add a given script tag on their webpage to the bottom of the body tag under my index.html.
+- A new partials folder was an important asset of the plug in to add my own styling.
+- Which also meant I had to include another @import tag in the styles.scss folder (@import 'partials/selectric';)
+- Under _selectric.scss (the new partials folder) is where all the styling was added. 
+ 
 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function () {
 
   $('select').selectric();
 
-  $('.section-list').on('change', function(){
+  $('.section-list').on('change', function () {
     const category = $(this).val();
     const $loadGif = $('.loading-gif');
     $('header').addClass('active-header');
@@ -11,32 +11,33 @@ $(function(){
     $('select').selectric();
     $.ajax({
       method: 'get',
-      url: 'https://api.nytimes.com/svc/topstories/v2/'+ category + '.json?api-key=n8C2eF8jDItIXtbM7nYFtYbPiZax89hp'
+      url: 'https://api.nytimes.com/svc/topstories/v2/' + category + '.json?api-key=n8C2eF8jDItIXtbM7nYFtYbPiZax89hp'
     })
-    .done(function(data){ // Start of done function()
-      const filterData = data.results.filter(function(article){
-        return article.multimedia[4] !== undefined;
-      });
-      const articleLink = filterData.slice(0,12);
-      const $mainLink = $('.main-section');
-      $mainLink.html('');
-      $.each(articleLink, function(index, article) {
-        const articleUrl = article.url;
-        const articleImage = article.multimedia[4].url;
-        const newsTemplate = `<a href="${articleUrl}" class="news-item-container">
+      .done(function (data) { // Start of done function()
+        const filterData = data.results.filter(function (article) {
+          return article.multimedia[4] !== undefined;
+        });
+        const articleLink = filterData.slice(0, 12);
+        const $mainLink = $('.main-section');
+        $mainLink.html('');
+        $.each(articleLink, function (index, article) {
+          const articleUrl = article.url;
+          const articleImage = article.multimedia[4].url;
+          const newsTemplate = `<a href="${articleUrl}" class="news-item-container">
         <img class="article-image" src="${articleImage}">
         <div class="text-block">
         <p class="text-block-p">${article.abstract}</p>
         </div>
         </a>`;
-        $mainLink.append(newsTemplate);
-      });// End of each
-    }) // End of done function()
-    .fail(function(){
-    }) // End of fail function()
-    .always(function(){
-      $loadGif.hide();
-    }) // End of always function()
+          $mainLink.append(newsTemplate);
+        });// End of each
+      }) // End of done function()
+      .fail(function () {
+        alert('Sorry there was an error!');
+      }) // End of fail function()
+      .always(function () {
+        $loadGif.hide();
+      }) // End of always function()
   }); // End of click function ()
 
 }); // End of jQuery doc ready
